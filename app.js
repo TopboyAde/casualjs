3dc1a60d417bf05fa89027b01f5b4d183b9f4679
ade <adeadewuyi@gmail.com>
2022-11-08 22:11:59 +0100

    new commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// const move = document.querySelector('.middle')
+// console.log(move)
+// move.style.BackgroundColor = 'red'
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggledarkmode("dark-mode");
+}
